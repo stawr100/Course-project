476eee9fbeccb630577ddeab27cce5379e42a3a0
stawr100 <sstawr@gmail.com>
2016-11-15 22:19:23 +0300

    New color

--- Course project/Form1.Designer.cs
+++ Course project/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
             this.ClientSize = new System.Drawing.Size(836, 427);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
